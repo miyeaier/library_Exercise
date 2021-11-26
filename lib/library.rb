9622e9146ad67480eeb23fa9ccce8aaef5a474d6
miyeaier <miyesier@gmail.com>
2021-11-26 14:56:27 +0100

    add checkout mothed

--- lib/library.rb
+++ lib/library.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require "date"
 
 class Library
   attr_accessor :books
@@ -7,7 +8,20 @@ class Library
     @books = YAML.load_file("./lib/data.yml")
   end
 
-  def search
+  def search(title)
+    @books.detect { |object| object[:book][:title] == title }
+  end
+  
+  def checkout(object,person)
+    let(:person){ instance_double('Person',book_shelf: )}
+    #set the attributes
     
+    object["available"] = false
+    object["return_date"] = Date.today.next_month.strftime("%Y-%m-%d")
+    
+    #update the persons #book_shelf
+    person.book_shelf.push(object)
+    
+    #write to the yaml file
   end
 end
--- spec/library.spec.rb
+++ spec/library.spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Library do
   describe "instance methods" do
     it { is_expected.to respond_to(:books) }
     it { is_expected.to respond_to(:search) }
+    it { is_expected.to respond_to(:checkout) }
   end
   it "is expected to have a collection of books as an array" do
     expect(subject.books).to be_instance_of Array
@@ -19,6 +20,36 @@ RSpec.describe Library do
     end
     it "is expected to have a return date" do
       expect(subject.books.first.keys).to include("return_date")
+    end
+  end
+
+  describe "#search" do
+    describe "using title" do
+      it "is expected to return one object" do
+        book = subject.search("Lord of the flies")
+        expected_result = {
+          :book => {
+            :title => "Lord of the flies",
+            :author => "William Golding",
+          },
+          "available" => true,
+          "return_date" => "",
+        }
+        expect(book).to eq(expected_result)
+      end
+    end
+  end
+  describe "#checkout" do
+    before do
+      @book = subject.search("Tuesdays with Morrie")
+      subject.checkout(@book,person)
+    end
+    it "is expected to set availability to false" do
+      expect(@book["available"]).to eq false
+    end
+    it "is expected to set return date to today + 30 days" do
+    expected_return_date = Date.today.next_month.strftime("%Y-%m-%d")
+    expect(@book['return_date']).to eq expected_return_date
     end
   end
 end
